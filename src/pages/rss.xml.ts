4effcaeb5284dd38801cbe04f9ad9d831c7d9820
Martin Ryberg Laude <martin.ryberg.laude@gmail.com>
2023-07-03 18:07:52 +0200

    fix: rss issue

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,31 +1,34 @@
 import rss from "@astrojs/rss";
 import sanitizeHtml from "sanitize-html";
-import MarkdownIt from "markdown-it";
-import { fetchApi } from "../lib/strapi";
+import fetchApi from "../lib/strapi";
+import { Marked } from "marked";
+import type Post from "../interfaces/post";
 
-const parser = new MarkdownIt();
 
-export async function get(context) {
-  const posts = await fetchApi({
+export async function get(context: any) {
+  const marked = new Marked(
+    { mangle: false }
+  );
+  const posts = await fetchApi<Post[]>({
     endpoint: "devposts",
     wrappedByKey: "data",
   });
   posts.sort((a, b) => {
-      return (
-        new Date(b.attributes.pubDate).getTime() -
-        new Date(a.attributes.pubDate).getTime()
-      );
-    })
+    return (
+      new Date(b.attributes.pubDate).getTime() -
+      new Date(a.attributes.pubDate).getTime()
+    );
+  })
   return rss({
     title: "Martin R. Laude's Blog",
     description: "Blog covering my journey in web development",
     site: context.site,
-    items: blog.map((post) => ({
+    items: posts.map((post) => ({
       title: post.attributes.title,
       pubDate: post.attributes.pubDate,
       description: post.attributes.description,
       link: `/blog/${post.attributes.slug}/`,
-      content: sanitizeHtml(parser.render(post.attributes.content)),
+      content: sanitizeHtml(marked.parse(post.attributes.content)),
     })),
   });
 }
