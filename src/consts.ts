0204275bdce4d6bd599bdcd9ea8726a51aef7daa
Martin Ryberg Laude <martin.ryberg.laude@gmail.com>
2025-03-31 00:03:45 +0200

    feat: add posts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Martin Ryberg Laude";
+export const SITE_DESCRIPTION =
+  "Swedish guy passionate about software, design, and anything inbetween.";
