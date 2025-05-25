007481623667406060fe0686438e7eb2cf7202ff
Martin Ryberg Laude <martin.ryberg.laude@gmail.com>
2025-05-25 12:17:57 +0200

    fix: site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://mrlaude.com",
   integrations: [mdx(), sitemap()],
 
   vite: {
